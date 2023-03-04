eb8a80777825f6cb314a6f5462f4d3404f2bb6ce
Mojtaba-z <mojtaba.zolfaghari30@gmail.com>
2023-03-04 20:08:07 +0330

    update

--- client/views.py
+++ client/views.py
@@ -28,19 +28,23 @@ class ReservationViewSet(viewsets.ModelViewSet):
         # contains userprofile object
         userprofile = UserProfile.objects.get(user__id=1)  # we should use request.user to identify user
         request.data['guest_id'] = userprofile.id
-        room_ids = request.data.pop('rooms_id')
+
+        room_ids = request.data.pop('rooms_id') if 'rooms_id' in request.data.keys() else None
 
         # send data to serializer
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             reservation_obj = serializer.create(validated_data=request.data)  # contains agent listing object
-            rooms_list = [
-                item for item in Room.objects.filter(id__in=room_ids) if item in reservation_obj.property.room.all()
-            ]
-            if rooms_list:
-                Room.objects.filter(id__in=room_ids).update(room_status="reserved")
-            else:
-                return Response({'message': "room is not for this property"}, status=status.HTTP_404_NOT_FOUND)
+            if room_ids is not None:
+                rooms_list = [
+                    item for item in Room.objects.filter(id__in=room_ids) if item in reservation_obj.property.room.all()
+                ]
+                if rooms_list:
+                    Room.objects.filter(id__in=room_ids).update(room_status="reserved")
+                else:
+                    return Response({'message': "room is not for this property"}, status=status.HTTP_404_NOT_FOUND)
+                reservation_obj.property.property_status = "room_reserve"  # change property status
+                reservation_obj.property.save()
             serializer = self.serializer_class(reservation_obj)
             return Response({'result': serializer.data}, status=status.HTTP_201_CREATED)
         return Response({'result': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
